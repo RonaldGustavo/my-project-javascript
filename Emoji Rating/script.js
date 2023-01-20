eed8c79252074f16701e7a734f0ea36f667365c2
Ronald Gustavo <Ronald.gustavo@muf.co.id>
2023-01-20 17:19:16 +0700

    update project Emoji Rating

--- Emoji Rating/script.js
+++ Emoji Rating/script.js
@@ -2,9 +2,12 @@ const starEl = document.querySelectorAll(".fa-star");
 
 const emojisEl = document.querySelectorAll(".fa-regular");
 
+const btnSubmit = document.querySelector(".btn-submit")
+
 starEl.forEach((starEl, index) => {
     starEl.addEventListener("click", () => {
         updateRating(index);
+        
     });
 });
 
@@ -17,6 +20,13 @@ function updateRating(index) {
         }
     });
     emojisEl.forEach((emojiEl) => {
-        emojiEl.style.transform = 'translateX( -$ {index * 50}px)';
+        emojiEl.style.transform = "translateX(" + index * -50 + "px)";
     });
 }
+
+btnSubmit.addEventListener("click", ()=> {
+            alert("Thank Your for Submit")
+            location.reload();
+    
+})
+
