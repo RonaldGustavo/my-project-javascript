371f6f14c7ed12107156906166610d84cb70b3dc
Ronald Gustavo <Ronaldgustavo.rg@Gmail.com>
2023-01-27 22:14:57 +0700

    Create Project Random Quotes API

---
+++ Random Quotes API/script.js
@@ -0,0 +1,39 @@
+const btnEl = document.getElementById("btn")
+
+const quoteEl = document.getElementById("quote")
+const authorEl = document.getElementById("author")
+
+const apiURL = "https://api.quotable.io/random"
+
+async function getQuote() {
+
+    try {
+        btnEl.innerText = "Loading....."
+        btnEl.disabled = true
+        quoteEl.innerText = "Loading data"
+        authorEl.innerText = "Loading data"
+        const response = await fetch(apiURL)
+        const data = await response.json()
+        const quoteContent = data.content;
+        const quoteAuthor = data.author;
+        quoteEl.innerText = quoteContent
+        authorEl.innerText = "~ " + quoteAuthor
+        btnEl.innerText = "Get a Quote"
+        btnEl.disabled = false
+        console.log(data);
+
+    } catch (error) {
+        console.log(error)
+        //when offline or error
+        quoteEl.innerText = "an error happened, try again later!"
+        authorEl.style.visibility = "hidden"
+        btnEl.disabled = false
+
+    }
+
+}
+
+// automatic refresh quotes
+getQuote()
+
+btnEl.addEventListener("click", getQuote)
